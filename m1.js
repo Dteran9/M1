7a30f9349784cb2c08913172f02e5556caa0e6f2
Deanndra <97985953+Deanndra@users.noreply.github.com>
2023-09-05 13:11:13 -0500

    created js variable

--- m1.js
+++ m1.js
@@ -1,1 +1,19 @@
-console.log('Hello Professor Escobar! What was most exciting activity you did this summer?') ;
+const m1 = 100; // this is my module1 life bar  NUMBER
+
+m1 = m1 - 10;
+
+//90
+
+m1 = m1 - 10;
+
+var name = "Module 1"; // this is my assignment name  STRING
+
+var checkout = true; // BOOLEAN
+
+{
+    var x = 2; // BLOCK SCOPE VARIABLE
+}
+
+
+
+console.log(m1);
